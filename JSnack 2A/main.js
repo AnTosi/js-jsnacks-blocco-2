ee6650efe3bf110c416615b482ed892161cc2169
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-25 12:40:27 +0200

    fatto JSnack2

--- JSnack 2A/main.js
+++ JSnack 2A/main.js
@@ -1,31 +1,39 @@
 const teams = [
     {
         "nome": "Juventus",
-        "punti fatti": 0,
-        "falli subiti": 0  
+        "punti": 0,
+        "falli": 0  
     },
 
     {
         "nome": "Inda",
-        "punti fatti": 0,
-        "falli subiti": 0  
+        "punti": 0,
+        "falli": 0  
     },
 
     {
         "nome": "Bilan",
-        "punti fatti": 0,
-        "falli subiti": 0  
+        "punti": 0,
+        "falli": 0  
     },
 
     {
         "nome": "Riomma",
-        "punti fatti": 0,
-        "falli subiti": 0  
+        "punti": 0,
+        "falli": 0  
     },
 
     {
         "nome": "Lazzie",
-        "punti fatti": 0,
-        "falli subiti": 0  
+        "punti": 0,
+        "falli": 0  
     },
 ]
+
+for (let i = 0; i < teams.length; i++) {
+    const team = teams[i];
+    team.punti = Math.floor(Math.random() * (100 - 0) ) + 0;
+    team.falli = Math.floor(Math.random() * (100 - 0) ) + 0;
+    console.log(team);
+    
+}

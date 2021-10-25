6efb7b65594703781279c94b92c668a75c007cf3
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-25 11:53:38 +0200

    stampato a schermo il peso, ora devo stampare bici

---
+++ main.js
@@ -0,0 +1,36 @@
+let weight = [];
+const bike = [
+    { 
+    "nome":"cannondale",
+    "peso": 10
+},
+{
+    "nome": "atala",
+    "peso": 12 
+},
+
+{
+    "nome": "brumotti special",
+    "peso": 5
+}
+
+
+]
+
+
+for (let i = 0; i < bike.length; i++) {
+
+    let peso = (bike[i].peso);
+    // console.log(parseInt(bike[i].peso));
+    weight.push(peso);
+    // console.log(weight);
+}
+
+
+let minWeight = Math.min(...weight);
+
+
+console.log(Math.min(...weight));
+// console.log(Math.min(parseInt(...peso)));
+
+document.querySelector("h1").innerHTML = Math.min(...weight);

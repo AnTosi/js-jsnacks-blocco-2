be5b825ac862d874fbfdb3a36d826eb9f371c854
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-25 12:20:40 +0200

    fattto?

---
+++ JSnack 2A/main.js
@@ -0,0 +1,31 @@
+const teams = [
+    {
+        "nome": "Juventus",
+        "punti fatti": 0,
+        "falli subiti": 0  
+    },
+
+    {
+        "nome": "Inda",
+        "punti fatti": 0,
+        "falli subiti": 0  
+    },
+
+    {
+        "nome": "Bilan",
+        "punti fatti": 0,
+        "falli subiti": 0  
+    },
+
+    {
+        "nome": "Riomma",
+        "punti fatti": 0,
+        "falli subiti": 0  
+    },
+
+    {
+        "nome": "Lazzie",
+        "punti fatti": 0,
+        "falli subiti": 0  
+    },
+]
